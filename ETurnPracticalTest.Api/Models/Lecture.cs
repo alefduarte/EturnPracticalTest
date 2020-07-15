91032e7ebc27f7898dfa96efb06519bbadcda3d9
Alef Duarte <alef.duarte@outlook.com>
2020-07-14 22:51:24 -0300

    Added Lecture Model

---
+++ ETurnPracticalTest.Api/Models/Lecture.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ETurnPracticalTest.Api.Models
+{
+    public class Lecture
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+    }
+}
