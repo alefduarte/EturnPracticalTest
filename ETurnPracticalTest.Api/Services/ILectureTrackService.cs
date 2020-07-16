96d938dfececf608c68b164f60fbbf223965e7e8
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 13:51:53 -0300

    Created ILectureTrackService for Track x Lecture one-to-many relationship

---
+++ ETurnPracticalTest.Api/Services/ILectureTrackService.cs
@@ -0,0 +1,16 @@
+﻿using ETurnPracticalTest.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETurnPracticalTest.Api.Services
+{
+    public interface ILectureTrackService
+    {
+        ServiceResponse<IEnumerable<LectureTrackResponse>> AllocateLectures();
+        ServiceResponse<IEnumerable<LectureTrack>> GetLectureTracks();
+        void AddLecture(Lecture lecture);
+        ServiceResponse<LectureTrack> GetLectureTrack(int id);
+    }
+}
