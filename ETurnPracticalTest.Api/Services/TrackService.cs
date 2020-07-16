2aa5a8fc84a1be60ce2c3daa8ba55eeffc3af12e
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 02:41:12 -0300

    Implemented ITrackService

---
+++ ETurnPracticalTest.Api/Services/TrackService.cs
@@ -0,0 +1,33 @@
+﻿using ETurnPracticalTest.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETurnPracticalTest.Api.Services
+{
+    public class TrackService : ITrackService
+    {
+        protected static List<Track> TracksList { get; }
+
+        static TrackService()
+        {
+            TracksList = new List<Track>();
+        }
+
+        public Track GetTrack(int id)
+        {
+            return TracksList.Where(record => record.Id == id).FirstOrDefault();
+        }
+
+        public void StoreTrack(Track track)
+        {
+            TracksList.Add(track);
+        }
+
+        public IEnumerable<Track> GetTracks()
+        {
+            return TracksList;
+        }
+    }
+}
