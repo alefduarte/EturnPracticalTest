ab881fcf33b2f0f297a35c352a942b01ddc56b2e
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 10:22:23 -0300

    Added Track property and overload and added default 0 value to Duration

--- ETurnPracticalTest.Api/Models/Lecture.cs
+++ ETurnPracticalTest.Api/Models/Lecture.cs
@@ -6,7 +6,8 @@ namespace ETurnPracticalTest.Api.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int Duration { get; set; }
+        public int Duration { get; set; } = 0;
+        public Track Track { get; set; }
 
         public Lecture() { }
 
@@ -20,6 +21,15 @@ namespace ETurnPracticalTest.Api.Models
         {
             Duration = duration;
         }
-    }
 
+        public Lecture(int id, string title, int duration, Track track) : this(id, title, duration)
+        {
+            Track = track;
+        }
+
+        public void AttachTrack(Track track)
+        {
+            Track = track;
+        }
+    }
 }
