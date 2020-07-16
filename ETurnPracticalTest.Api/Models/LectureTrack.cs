6931fe663ce48ee736d6eeb03a227e77bc104494
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 02:33:56 -0300

    Implemented LectureTrack model

---
+++ ETurnPracticalTest.Api/Models/LectureTrack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETurnPracticalTest.Api.Models
+{
+    public class LectureTrack
+    {
+        public Lecture Lecture { get; set; }
+        public Track Track { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan? EndTime { get; set; }
+
+        public LectureTrack(Lecture lecture, Track track, TimeSpan startTime, TimeSpan? endTime)
+        {
+            Lecture = lecture;
+            Track = track;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    }
+}
