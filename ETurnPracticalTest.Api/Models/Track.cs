e7acbc60450789d1a3ebbf2e0b1ea0b2d3f3d7dc
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 02:33:08 -0300

    Implemented track model

---
+++ ETurnPracticalTest.Api/Models/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETurnPracticalTest.Api.Models
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Track(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
