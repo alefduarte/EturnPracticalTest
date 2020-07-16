07c7886d09727a11ff487ddf9c731d603cdf0df6
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 02:32:48 -0300

    Added new props and constructor

--- ETurnPracticalTest.Api/Models/Lecture.cs
+++ ETurnPracticalTest.Api/Models/Lecture.cs
@@ -4,7 +4,22 @@ namespace ETurnPracticalTest.Api.Models
 {
     public class Lecture
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public int Duration { get; set; }
+
+        public Lecture() { }
+
+        public Lecture(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public Lecture(int id, string title, int duration) : this(id, title)
+        {
+            Duration = duration;
+        }
     }
+
 }
