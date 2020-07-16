8f5af6c3f611ceeae480f358a2d26a9169673962
Alef Duarte <alef.duarte@outlook.com>
2020-07-16 10:24:22 -0300

    Injected LectureService and TrackService

--- ETurnPracticalTest.Api/Startup.cs
+++ ETurnPracticalTest.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ETurnPracticalTest.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,13 +23,13 @@ namespace ETurnPracticalTest.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ILectureService, LectureService>();
+            services.AddScoped<ITrackService, TrackService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
